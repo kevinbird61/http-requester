a9507def8dbd71ff2f09513f972d858b614383cb
kevinbird61 <kevinbird61@gmail.com>
2019-11-29 08:12:31 +0800

    Fix test case

--- test/test_http_state_machine_2.c
+++ test/test_http_state_machine_2.c
@@ -12,25 +12,37 @@ int main(int argc, char *argv[])
     http_req_obj_create(&http_req);
     char *http_request;
     // start-line
-    http_req_create_start_line(&http_request, "GET", argv[3], HTTP_1_1);
+    http_req_create_start_line(&http_request, "GET", "/", HTTP_1_1);
     // header fields
     http_req_obj_ins_header_by_idx(&http_req, REQ_HOST, argv[1]);
     http_req_obj_ins_header_by_idx(&http_req, REQ_CONN, "keep-alive");
     http_req_obj_ins_header_by_idx(&http_req, REQ_USER_AGENT, "http-request-c");
-
     for(int i=1;i<REQ_HEADER_NAME_MAXIMUM;i++){
         if(http_req->dirty_bit_align& (1<<(i-1)) ){
-            //puts(http_req->field_value[i]);
             http_req_ins_header(&http_request, get_req_header_name_by_idx[i], http_req->field_value[i]);
         }
     }
-
     http_req_finish(&http_request);
-    char *ori_req=strdup(http_request);
-    http_request=realloc(http_request, 2*strlen(http_request)+8);
-    sprintf(http_request, "%s\r\n\r\n%s\r\n\r\n", ori_req, ori_req);
+
+    char *http_request2;
+    // start-line
+    http_req_create_start_line(&http_request2, "GET", argv[3], HTTP_1_1);
+    // header fields
+    http_req_obj_ins_header_by_idx(&http_req, REQ_HOST, argv[1]);
+    http_req_obj_ins_header_by_idx(&http_req, REQ_CONN, "keep-alive");
+    http_req_obj_ins_header_by_idx(&http_req, REQ_USER_AGENT, "http-request-c");
+    for(int i=1;i<REQ_HEADER_NAME_MAXIMUM;i++){
+        if(http_req->dirty_bit_align& (1<<(i-1)) ){
+            http_req_ins_header(&http_request2, get_req_header_name_by_idx[i], http_req->field_value[i]);
+        }
+    }
+    http_req_finish(&http_request2);
+
+    char *total_reqs;
+    total_reqs=calloc(strlen(http_request2)+strlen(http_request)+8, 1);
+    sprintf(total_reqs, "%s\r\n\r\n%s\r\n\r\n", http_request, http_request2);
     printf("HTTP request:*******************************************************************\n");
-    printf("%s\n", http_request);
+    printf("%s\n", total_reqs);
     printf("================================================================================\n");
 
     // create connection
@@ -40,7 +52,7 @@ int main(int argc, char *argv[])
     }
 
     void *res;
-    send(sockfd, http_request, strlen(http_request), 0);
+    send(sockfd, total_reqs, strlen(total_reqs), 0);
     http_rcv_state_machine(sockfd, &res);
     http_rcv_state_machine(sockfd, &res);
 
